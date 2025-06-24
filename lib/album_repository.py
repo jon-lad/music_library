ed3b2cf8ddf9c4eb5c9be7d0ff596c44fd705276
Jonathan Ward <jon_lad@hotmail.com>
2025-06-24 16:06:11 +0100

    added create and delete methods for albums

--- lib/album_repository.py
+++ lib/album_repository.py
@@ -18,3 +18,17 @@ class AlbumRepository:
         rows = self.connection.execute('SELECT * FROM albums WHERE id = %s', [album_id])
         row = rows[0]
         return Album(row["id"], row["title"], row["release_year"], row["artist_id"])
+    
+    def create(self, album):
+        self.connection.connect()
+        self.connection.execute("""INSERT INTO albums (title, release_year, artist_id) 
+                                VALUES (%s, %s, %s)""",
+                                [
+                                    album.title,
+                                    album.release_year,
+                                    album.artist_id
+                                ])
+        
+    def delete(self, id):
+        self.connection.connect()
+        self.connection.execute("DELETE FROM albums WHERE id = %s", [id])
