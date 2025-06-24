61b15106c0cc0b0ece0c4bdcb195c741c89f275f
Jonathan Ward <jon_lad@hotmail.com>
2025-06-24 16:29:03 +0100

    added application wrapper

--- app.py
+++ app.py
@@ -2,32 +2,37 @@ from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
+class Aplication:
+    def __init__(self):
+        # Connect to the database
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        # Seed with some seed data
+        self._connection.seed("seeds/music_library.sql")
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+    def run(self):
+        print("Welcome to the music library manager!\n")
+        print("What would you like to do?")
+        print(" 1 - List all albums")
+        print(" 2 - List all artists\n")
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+        choice = input("Enter your choice: ")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+        if choice == '1':
+            # Retrieve all albums
+            album_repository = AlbumRepository(self._connection)
+            albums = album_repository.all()
+            print("Here is a list of albums:")
+            for i, album in enumerate(albums, 1):
+                print(f" * {i} - {album.title}")
+        else:
+            # Retrieve all artists
+            artist_repository = ArtistRepository(self._connection)
+            artists = artist_repository.all()
+            print("Here is a list of artists:")
+            for i, artist in enumerate(artists, 1):
+                print(f" * {i} - {artist.name}")
 
-# List them out
-for artist in artists:
-    print(artist)
-
-# Find a single artist
-print(artist_repository.find(1))
-
-# Retrieve all albums
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
-
-# List them out
-for album in albums:
-    print(album)
-
-# Find a single album
-print(album_repository.find(1))
+if __name__ == '__main__':
+    app = Aplication()
+    app.run()
